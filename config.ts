b167c7cb39a16504be3850057719181938a1090a
PromiSe#### <vicimpa@ya.ru>
2021-10-02 13:26:55 +0000

    --wip-- [skip ci]

--- config.ts
+++ config.ts
@@ -1,35 +1,33 @@
 import { join } from "path"
 import { timeparser } from "~/lib/timeparser"
 
-export const guildId = '805944675188867112'
-export const rolesChannel = '807067262569283645'
+export const guildId = ''
+export const rolesChannel = ''
 
 import ctl from "./data/ctl.json"
 import cmd from "./data/cmd.json"
 import qiwi from "./data/qiwi.json"
 
-export const privates = [
-  '808155036910616596'
-]
+/** @type {string[]} */
+export const privates = []
 
-export const actions = [
-  '821871687439220737'
-]
+/** @type {string[]} */
+export const actions = []
 
 export const logs = {
-  logs: '827688541932617738',
-  jail: '807670197740896326',
-  report: '805944675743694910',
-  admin:'805944675533717560',
-  dmbot: '805944675743694911',
-  news: '807581614821081090',
-  pools: '807592657814945793',
-  roles: '807593669985173504',
-  battle: '807593935799189504',
-  donations: '808458677727264819',
-  voice: '808427116730515456',
-  fines: '810114604721766420',
-  bad: '829684581459558400'
+  logs: '',
+  jail: '',
+  report: '',
+  admin:'',
+  dmbot: '',
+  news: '',
+  pools: '',
+  roles: '',
+  battle: '',
+  donations: '',
+  voice: '',
+  fines: '',
+  bad: ''
 }
 
 export const createdName = 'Create room'
@@ -41,19 +39,23 @@ export const commandsPath = {
 }
 
 export const mutes = {
-  chat: '805944675243917365',
-  voice: '805944675243917366'
+  chat: '',
+  voice: ''
 }
 
-export const onlyMedia = [
-  '813158914186805328'
-]
+/**
+ * @type {string[]}
+ */
+export const onlyMedia = []
 
-export const sponsors = [
-  {id: '805944675235397680', price: 150, name: 'Sponsor'},
-  {id: '805944675235397681', price: 250, name: 'Sponsor +'},
-  {id: '805944675243917362', price: 500, name: 'Sponsor XL'}
-]
+/**
+ * @type {{
+ *  id: string,
+ *  price: number,
+ *  name: string
+ * }[]}
+ */
+export const sponsors = []
 
 export const blockLimit = 30
 
@@ -71,7 +73,7 @@ export const webPort = 4001
 export const qiwiHost = '127.0.0.2'
 export const qiwiPort = 4002
 
-export const mongoUrl = 'mongodb://127.0.0.2:65017/hellcord'
+export const mongoUrl = 'mongodb://127.0.0.2:27017/server'
 
 export const qiwiPublic = qiwi.public
 export const qiwiPrivate = qiwi.private
